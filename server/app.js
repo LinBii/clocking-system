c7ca44ca798c4237affbabf52f6d90118567f442
LinBii <aa68968@gmail.com>
2022-12-18 20:28:50 +0800

    modify express routes

--- server/app.js
+++ server/app.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('./models');
+const routes = require('./routes');
 
-const Clocking = db.Clocking;
-const User = db.User;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(routes);
 
 app.listen(port, () =>
   console.log(`Clock-in server listening on port ${port}`)
