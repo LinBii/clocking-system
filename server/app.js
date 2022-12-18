e2354708866dd0db4b75344ef31973fe082db284
LinBii <aa68968@gmail.com>
2022-12-18 21:42:23 +0800

    add signup api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes');
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(port, () =>
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const home = require('./modules/home');
+const userController = require('../controllers/user-controller');
+const { apiErrorHandler } = require('../middleware/error-handler');
 
-router.use('/', home);
+router.post('/signup', userController.signUp);
+
+router.use('/', home, apiErrorHandler);
 
 module.exports = router;
