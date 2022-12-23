d4671601e78cecb945af13055595f51fc23cc1bd
LinBii <aa68968@gmail.com>
2022-12-23 20:33:45 +0800

    fix user model

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Clocking);
+      User.hasMany(models.Attendance);
     }
   }
   User.init(
