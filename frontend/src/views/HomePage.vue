9b4b7af63e6ec8b5ca2ccaa89a8088ab50ba81cd
LinBii <aa68968@gmail.com>
2022-12-23 09:57:41 +0800

    add clock-in/out interaction and deal with day change issue

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1>Welcome to the Clock-In App</h1>
-    <p>Current time: {{ currentTime }}</p>
-    <button @click="clockIn">Clock In</button>
-    <button @click="clockOut">Clock Out</button>
+    <h1>歡迎來到PUNCHIN！</h1>
+    <p>現在時間： {{ currentTime }}</p>
+    <button :disabled="clockedIn" @click="clockIn">打卡上班</button>
+    <button :disabled="!clockedIn" @click="clockOut">打卡下班</button>
     {{ clockInTime }}
     {{ clockOutTime }}
   </div>
@@ -18,6 +18,9 @@ export default {
     const clockInTime = ref(null);
     const clockOutTime = ref(null);
 
+    // check if the user is clocked in
+    const clockedIn = ref(false);
+
     const elapsedTime = computed(() => {
       if (clockInTime.value && clockOutTime.value) {
         return clockOutTime.value - clockInTime.value;
@@ -27,14 +30,40 @@ export default {
 
     function clockIn() {
       clockInTime.value = new Date();
+
+      // able the clock-out button, disable the clock-in button
+      clockedIn.value = true;
     }
 
     function clockOut() {
       clockOutTime.value = new Date();
+
+      // Set the clockOutTime ref to the current time, if it is later than the current value
+      if (!clockOutTime.value || new Date() > clockOutTime.value) {
+        clockOutTime.value = new Date();
+
+        clockedIn.value = true;
+      }
     }
 
+    // Update currentTime ref every second
     setInterval(() => {
       currentTime.value = new Date();
+
+      // Check if it is past the day change time (GMT+8 05:00)
+      const dayChangeTime = new Date(
+        currentTime.value.getFullYear(),
+        currentTime.value.getMonth(),
+        currentTime.value.getDate(),
+        5,
+        0,
+        0
+      );
+      if (currentTime.value > dayChangeTime) {
+        clockOutTime.value = null;
+
+        clockedIn.value = false;
+      }
     }, 1000);
 
     return {
@@ -42,6 +71,7 @@ export default {
       clockInTime,
       clockOutTime,
       elapsedTime,
+      clockedIn,
       clockIn,
       clockOut,
     };
