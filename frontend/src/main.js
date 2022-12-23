8b2d613af28ff1f81f8770a225d04571c7372417
LinBii <aa68968@gmail.com>
2022-12-23 21:30:04 +0800

    add vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
