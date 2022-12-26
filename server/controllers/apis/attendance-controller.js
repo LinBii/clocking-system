f686ce9aab4142735c00e3bbf4c3f6f79faf98b7
LinBii <aa68968@gmail.com>
2022-12-26 13:47:06 +0800

    add postAttendance and updateAttendance functions

---
+++ server/controllers/apis/attendance-controller.js
@@ -0,0 +1,60 @@
+const { Attendance } = require('../../models');
+
+let attendanceController = {
+  postAttendance: (req, res, next) => {
+    if (!req.body.date || !req.body.clockIn) {
+      return res.json({
+        status: 'error',
+        message: 'attendance time not exist!',
+      });
+    }
+    return Attendance.create({
+      userId: req.user.id,
+      date: req.body.date,
+      clockIn: req.body.clockIn,
+    })
+      .then((attendance) => {
+        console.log('attendance', attendance);
+        return res.json({
+          status: 'success',
+          message: 'created new clock-in successfully',
+          attendanceId: attendance.id,
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+  updateAttendance: (req, res, next) => {
+    return Attendance.findByPk(req.params.id)
+      .then((attendance) => {
+        if (!attendance) {
+          return res.json({
+            status: 'error',
+            message: 'attendance record not found',
+          });
+        }
+        return Attendance.update({
+          clockOut: req.body.clockOut,
+        });
+      })
+
+      .then((result) => {
+        if (result[0] === 0) {
+          return res.json({
+            status: 'error',
+            message: 'attendance record not found',
+          });
+        }
+        return res.json({
+          status: 'success',
+          message: 'updated clock-out successfully',
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+};
+
+module.exports = attendanceController;
