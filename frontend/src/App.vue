d0ab6a242a4808cbbe1960a7cbd6a2c5283bab96
LinBii <aa68968@gmail.com>
2023-01-04 20:40:21 +0800

    add font-family

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,7 +17,8 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, 'Noto Sans CJK TC', 'Microsoft JhengHei', Helvetica,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
